d673d343544b7173b50088045f01314a8fffd20b
philbergdorf <phil.suter@gmail.com>
2025-06-06 12:17:16 +0200

    Fix Expo app configuration and dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
 import { Stack } from 'expo-router';
-import '../global.css'
+import '../global.css';
 import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
   useFrameworkReady();
+  
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
